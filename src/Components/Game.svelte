484d764ea1c7891e27656ab40f32f9aed8e2269c
Seth <s.zim517@gmail.com>
2020-07-19 13:46:15 -0400

    Adds logic for locking in chosen secret word (for other player)
    Will render MainGame component if all users are locked in

--- src/Components/Game.svelte
+++ src/Components/Game.svelte
@@ -1,10 +1,12 @@
 <script>
-    import { room, playerName } from '../store';
+    import { room, playerName, roomCodeState } from '../store';
+    import { db } from '../firebase.js';
     
     import FlexContainer from './FlexContainer.svelte';
     import Card from './Card.svelte';
     import Button from './Button.svelte';
     import Loader from './Loader.svelte';
+    import MainGame from './MainGame.svelte';
 
     const handleCardClick = (letter, i) => {
         if (maxLengthReached) {
@@ -19,8 +21,19 @@
         selectedIndexes = [];
     }
 
+    const lockInWord = async () => {
+        const dbRoomRef = db.collection('rooms').doc($roomCodeState);
+        const theRoom = await dbRoomRef.get();
+        const updatedPlayers = theRoom.data().players.map(player => 
+            player.name === $playerName ? ({ ...player, chosenSecretWord: { word, letters: word.split() } }) : ({ ...player }));
+        dbRoomRef.update({ players: updatedPlayers });
+        lockedIn = true;
+    }
+
     let word = '';
-    let maxLength = 4;
+    let maxLength = 5;
+    let lockedIn = false;
+    let allPlayersLockedIn = !$room.players.find(player => !player.chosenSecretWord);
     export let selectedIndexes = [];
     $: maxLengthReached = word.length === maxLength;
 </script>
@@ -55,7 +68,7 @@
 </style>
 
 <FlexContainer direction="column" justify="center" align="center">
-    {#if $room.players[0].initialCards}
+    {#if $room.players[0].initialCards && !allPlayersLockedIn}
         <div class="full-word-container">
             <div class="letters">
                 {#each word as singleLetter}
@@ -65,13 +78,18 @@
             {#if maxLengthReached}
                 <span style="color:red">Max word length reached</span>
             {/if}
-            <Button on:click={resetWord}>Reset word</Button>
+            <FlexContainer justify="space-around" width="320px">
+                <Button on:click={resetWord} disabled={lockedIn}>Reset word</Button>
+                <Button on:click={lockInWord} disabled={!maxLengthReached || lockedIn}>Lock in</Button>
+            </FlexContainer>
         </div>
         <div class="cards-container">
             {#each $room.players[0].initialCards as letter, i}
-                <Card {letter} disabled={maxLengthReached} on:click={(event) => handleCardClick(event.detail, i)} index={i} {selectedIndexes}/>
+                <Card {letter} disabled={maxLengthReached || lockedIn} on:click={(event) => handleCardClick(event.detail, i)}/>
             {/each}
         </div>
+    {:else if allPlayersLockedIn}
+        <MainGame />
     {:else}
         <Loader />
     {/if}
--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,6 @@ import { writable } from 'svelte/store';
 export const room = writable({
     players: []
 });
+export const roomCodeState = writable(null);
 export const playerName = writable(null);
 export const isRoomCreator = writable(false);
