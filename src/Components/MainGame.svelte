bf09f9490e454ce5c2546e3ab25b52bfce351e66
Seth <s.zim517@gmail.com>
2020-07-21 22:23:24 -0400

    THIS IS WHERE IT ALL HAPPENS!
    display for main game state

--- src/Components/MainGame.svelte
+++ src/Components/MainGame.svelte
@@ -1,10 +1,167 @@
 <script>
-    import { room, playerName } from '../store';
+    import { room, playerName, roomCodeState } from '../store';
+    import { db } from '../firebase.js';
     import Loader from './Loader.svelte';
+    import FlexContainer from './FlexContainer.svelte';
+    import Card from './Card.svelte';
+    import Button from './Button.svelte';
+    import ClueToken from './ClueToken.svelte';
+
+    let letterObjects = [];
+    let selectedIndexes = [];
+    let maxWordLength = false;
+    let confirmSuggestion
+
+    const handleCardClick = (letter, i, player) => {
+        if (letterObjects.length === 8) {
+            maxWordLength = true;
+            return;
+        }
+		letterObjects = [ ...letterObjects, { player, letter, index: i }];
+        selectedIndexes = [ ...selectedIndexes, i ];
+    }
+
+    const resetWord = () => {
+        maxWordLength = false;
+        letterObjects = [];
+        selectedIndexes = [];
+        confirmSuggestion = false;
+    }
+
+    const handleSuggest = () => {
+		if (letterObjects.length === 0) return;
+        confirmSuggestion = true;
+    }
+
+    const handleConfirmSuggestion = async () => {
+        confirmSuggestion = false;
+        const roomRef = db.collection('rooms').doc($roomCodeState);
+        let updatedCluesLeft = [ ...$room.cluesLeft ];
+        const updatedPlayers = $room.players.map(player => {
+            if (player.name !== $playerName) {
+                return player;
+            }
+            const clueColor = $room.cluesConfig.requiredPerPlayer > player.clues.length ? "red" : "green";
+            const index = updatedCluesLeft.indexOf(clueColor);
+            updatedCluesLeft.splice(index, 1);
+			const updatedClues = [ ...player.clues, clueColor ];
+			return { ...player, clues: updatedClues }
+        })
+        roomRef.update({ 
+			players: updatedPlayers,
+			cluesLeft: updatedCluesLeft,
+			currentClue: letterObjects
+		});
+		letterObjects = [];
+    }
 </script>
+
+<style>
+    .player-cards-container {
+        margin: 0 20px;
+    }
+
+    .full-word-container {
+        width: 100%;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .single-letter {
+        font-size: 80px;
+        text-decoration: underline;
+        margin: 0 8px;
+    }
+
+    .letters {
+        display: flex;
+        justify-content: center;
+    }
+</style>
 
 {#if $room.gamePhase !== 'main'}
     <Loader />
 {:else}
-    THA GAME
+    <FlexContainer width="100%" height="100%" direction="column">
+        <FlexContainer width="100%" height="5%" justify="center">
+            {#each $room.cluesLeft as clueColor}
+                <ClueToken color={clueColor} size="15px"/>
+            {/each}
+        </FlexContainer>
+        <FlexContainer width="100%" height="25%">
+            <div class="full-word-container">
+                <div class="letters">
+                    {#each letterObjects as { letter, player, index }, i}
+                        <div class="single-letter">{letter}</div>
+                        <div>{i + 1}</div>
+                    {/each}
+                </div>
+                <FlexContainer direction="column" align="center" width="320px">
+                    {#if maxWordLength}
+                        <span style="color:red">Max word length!</span>
+                    {:else if confirmSuggestion}
+                        <span style="color:red">This will use one of your team's clues!</span>
+                    {/if}
+                    <FlexContainer justify="space-around">
+                        <Button on:click={resetWord}>Reset word</Button>
+                        {#if !confirmSuggestion}
+                            <Button on:click={handleSuggest}>Suggest word</Button>
+                        {:else}
+                            <Button on:click={handleConfirmSuggestion}><span style="color:red">Are you sure?</span></Button>
+                        {/if}
+                    </FlexContainer>
+                </FlexContainer>
+            </div>
+        </FlexContainer>
+        <FlexContainer width="100%" height="65%" justify="center" align="center" wrap="wrap">
+            {#each $room.players as player}
+                <div class="player-cards-container">
+                    <FlexContainer align="center">
+                        <h3>{player.name}{player.name === $playerName ? " (Me)" : ""}</h3>
+                        {#if player.clues > 0}
+                            {#each player.clues as clueColor}
+                                <ClueToken color="{clueColor}" />
+                            {/each}
+                        {/if}
+                    </FlexContainer>
+                    <FlexContainer width="100%" justify="center" wrap="wrap">
+                        {#each player.secretWord.letters as letter, i}
+                            {#if player.name === $playerName}
+                                <Card />
+                            {:else if i === player.currentVisibleIndex}
+                                <Card
+									selected={letterObjects.find(obj => obj.index === i && obj.player === player.name)}
+									{letter}
+									on:click={(event) => handleCardClick(event.detail, i, player.name)}
+								/>
+                            {:else}
+                                <Card />
+                            {/if}
+                        {/each}
+                    </FlexContainer>
+                </div>
+            {/each}
+            <div style="margin:5px 0">
+                <FlexContainer wrap="wrap" justify="space-around" align="center">
+                    <FlexContainer direction="column" width="13%" justify="center" align="center">
+                        <h3>Wild</h3>
+                        <Card letter="*" on:click={(event) => handleCardClick(event.detail, '*', '*')}/>
+                    </FlexContainer>
+                    {#each Object.values($room.nonPlayerStands) as npc, i}
+                        <FlexContainer direction="column" width="13%" justify="center" align="center">
+                            <h3 style="text-align:center">{npc.length} left</h3>
+                            <Card 
+								selected={letterObjects.find(obj => obj.index === i && obj.player === 'NPC')}
+								letter={npc[0]}
+								on:click={(event) => handleCardClick(event.detail, i, 'NPC')}
+							/>
+                        </FlexContainer>
+                    {/each}
+                </FlexContainer>
+            </div>
+        </FlexContainer>
+    </FlexContainer>
 {/if}
