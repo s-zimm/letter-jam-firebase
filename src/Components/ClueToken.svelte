86e8cd16ec5a1bb923adb2c825de669f71a414fc
Seth <s.zim517@gmail.com>
2020-07-21 22:21:40 -0400

    clue token component

---
+++ src/Components/ClueToken.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let color = "";
+    export let size = "10px";
+</script>
+
+<style>
+    .clue-token {
+        border-radius: 50px;
+        margin: 5px;
+    }
+</style>
+
+<div class="clue-token" style="width:{size};height:{size};background-color:{color}"></div>
